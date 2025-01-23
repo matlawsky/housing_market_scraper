320164dc7b3a7cd25cc29e83b8fc6e96b428c376
matlawsky <matlawsky@gmail.com>
2025-01-23 12:12:31 +0100

    addin path arg to file hadling functions

--- file_handler.py
+++ file_handler.py
@@ -4,18 +4,18 @@ import os
 
 
 # reading newest file with links in a links folder
-def read_newest_links():
-    list_of_files = glob.glob("links/*.txt")
-    newes_file_name = max(list_of_files, key=os.path.getctime)
-    with open(newes_file_name, "r", encoding="utf-8") as file:
+def read_newest_links(path: str):
+    list_of_files = glob.glob(path)
+    newest_file_name = max(list_of_files, key=os.path.getctime)
+    with open(newest_file_name, "r", encoding="utf-8") as file:
         links = file.read().splitlines()
-    return links
+    return newest_file_name, links
 
 
 # handling offers stored in preliminary file
-def read_newest_offers():
-    list_of_files = glob.glob("offers/*.json")
+def read_newest_offers(path):
+    list_of_files = glob.glob(path)
     latest_file = max(list_of_files, key=os.path.getctime)
     with open(latest_file) as json_file:
         offers = json.load(json_file)
-    return offers
+    return latest_file, offers
